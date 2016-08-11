07d0865b0c23b1b130d08cafe38c74a48de6b3dc
wallaceiam <chris@mymojo.ca>
2016-08-11 16:52:03 +0100

    Started to add Tutorial project

---
+++ CQRS.Light.Tutorial/Commands/OpenTab.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Light.Tutorial.Commands
+{
+    public class OpenTab
+    {
+        public Guid TabId;
+        public int TableNumber;
+        public string Waiter;
+    }
+}
---
+++ CQRS.Light.Tutorial/Events/TabOpened.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Light.Tutorial.Events
+{
+    public class TabOpened
+    {
+        public Guid TabId;
+        public int TableNumber;
+        public string Waiter;
+    }
+}
